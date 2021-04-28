14cdbef3804a0d68663e0424477ba331870da9f2
Alisha <alisharao0749@gmail.com>
2021-04-28 19:19:19 +0530

    First Commit

---
+++ book.js
@@ -0,0 +1,118 @@
+  $(window).load(function(){
+         $(".flipbook-viewport").css("visibility","visible");
+     })
+ 
+ 
+ function gradientRight() {
+     
+     var i;
+     for (i = 2; i <=7; i = i + 2) {
+         $("#" + i).addClass("gradient-right");
+
+     }
+
+ }
+ gradientRight();
+
+ function gradientLeft() {
+    var i;
+     for (i = 3; i <=7; i = i + 2) {
+         $("#" + i).addClass("gradient-left");
+
+     }
+
+ }
+ gradientLeft();
+
+  var x = 0;  
+$(".zoom").click(function(){
+    x++;
+    if(x%2 != 0){
+        console.log('clicked');
+        $(".flipbook").css("transform", "scale(1)");
+    }
+    else{
+         $(".flipbook").css("transform", "scale(0.8)");
+    }
+    
+    
+    
+})
+
+
+
+
+ function loadApp() {
+     var book = $('.flipbook');
+    
+    if (book.width() == 0 || book.height() == 0) {
+						setTimeout(loadApp, 10);
+						return;
+					}
+
+     // Create the flipbook
+
+     $('.flipbook').turn({
+         // Width
+
+         width: 922,
+
+         // Height
+
+         height: 600,
+
+         // Elevation
+
+         elevation: 50,
+
+         // Enable gradients
+
+         gradients: true,
+
+         // Auto center this flipbook
+
+         autoCenter: true,
+
+     });
+    
+     $( ".flipbook" ).css("transform", "scale(0.8)");
+
+
+
+    //  $(".flipbook").bind("first", function(event) {
+    //      console.log('page 1 bitch');
+    //      $(this).addClass("translateCenter");
+
+    //  });
+    //   $(".flipbook").bind("last", function(event) {
+    //      console.log('page 1 bitch');
+    //      $(this).addClass("translateCenter");
+
+    //  });
+    
+
+
+
+     $(document).keydown(function (e) {
+
+         var previous = 37,
+             next = 39;
+
+         switch (e.keyCode) {
+             case previous:
+
+                 $('.flipbook').turn('previous');
+
+                 break;
+             case next:
+
+                 $('.flipbook').turn('next');
+
+                 break;
+         }
+
+     });
+    
+    
+ }
+ loadApp();
---
+++ index.js
@@ -0,0 +1,390 @@
+jQuery(document).ready(function($) {
+
+   $('.smoothscroll').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 800, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+  
+});
+
+
+
+TweenLite.defaultEase = Linear.easeNone;
+var ctrl = new ScrollMagic.Controller();
+
+
+$(".content").each(function(i) {
+  let target1 = $(this).find(".header");
+  let target2 = $(this).find(".copy");
+  var tl = new TimelineMax();
+  tl.from(target1, 0.5, { opacity: 0, x: 30, delay: 0.4 });
+  tl.from(target2, 0.5, { opacity: 0 }, "-=0.2");
+
+  new ScrollMagic.Scene({
+    triggerElement: this,
+    triggerHook: 0.5
+  })
+    .setTween(tl)
+    .addTo(ctrl)
+});
+
+
+$('#one').addClass("active");
+
+$(document).scroll(function () {
+  
+
+  // add class active to nav a on scroll
+  var scrollPos = $(document).scrollTop() + 100;
+  $('nav ul li a').each(function () {
+    var currLink = $(this);
+    var refElement = $(currLink.attr("href"));
+    if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+      $('nav ul li a').removeClass("active");
+      currLink.addClass("active");
+    }
+  });
+  
+});
+
+(function($){
+	
+	// Number of seconds in every time division
+	var days	= 24*60*60,
+		hours	= 60*60,
+		minutes	= 60;
+	
+	// Creating the plugin
+	$.fn.countdown = function(prop){
+		
+		var options = $.extend({
+			callback	: function(){},
+			timestamp	: 0
+		},prop);
+		
+		var left, d, h, m, s, positions;
+
+		// Initialize the plugin
+		init(this, options);
+		
+		positions = this.find('.position');
+		
+		(function tick(){
+			
+			// Time left
+			left = Math.floor((options.timestamp - (new Date())) / 1000);
+			
+			if(left < 0){
+				left = 0;
+			}
+			
+			// Number of days left
+			d = Math.floor(left / days);
+			updateDuo(0, 1, d);
+			left -= d*days;
+			
+			// Number of hours left
+			h = Math.floor(left / hours);
+			updateDuo(2, 3, h);
+			left -= h*hours;
+			
+			// Number of minutes left
+			m = Math.floor(left / minutes);
+			updateDuo(4, 5, m);
+			left -= m*minutes;
+			
+			// Number of seconds left
+			s = left;
+			updateDuo(6, 7, s);
+			
+			// Calling an optional user supplied callback
+			options.callback(d, h, m, s);
+			
+			// Scheduling another call of this function in 1s
+			setTimeout(tick, 1000);
+		})();
+		
+		// This function updates two digit positions at once
+		function updateDuo(minor,major,value){
+			switchDigit(positions.eq(minor),Math.floor(value/10)%10);
+			switchDigit(positions.eq(major),value%10);
+		}
+		
+		return this;
+	};
+
+
+	function init(elem, options){
+		elem.addClass('countdownHolder');
+
+		// Creating the markup inside the container
+		$.each(['Days','Hours','Minutes','Seconds'],function(i){
+			$('<span class="count'+this+'">').html(
+				'<span class="position">\
+					<span class="digit static">0</span>\
+				</span>\
+				<span class="position">\
+					<span class="digit static">0</span>\
+				</span>'
+			).appendTo(elem);
+			
+			if(this!="Seconds"){
+				elem.append('<span class="countDiv countDiv'+i+'"></span>');
+			}
+		});
+
+	}
+
+	// Creates an animated transition between the two numbers
+	function switchDigit(position,number){
+		
+		var digit = position.find('.digit')
+		
+		if(digit.is(':animated')){
+			return false;
+		}
+		
+		if(position.data('digit') == number){
+			// We are already showing this number
+			return false;
+		}
+		
+		position.data('digit', number);
+		
+		var replacement = $('<span>',{
+			'class':'digit',
+			css:{
+				top:'-2.1em',
+				opacity:0
+			},
+			html:number
+		});
+		
+		// The .static class is added when the animation
+		// completes. This makes it run smoother.
+		
+		digit
+			.before(replacement)
+			.removeClass('static')
+			.animate({top:'2.5em',opacity:0},'fast',function(){
+				digit.remove();
+			})
+
+		replacement
+			.delay(100)
+			.animate({top:0,opacity:1},'fast',function(){
+				replacement.addClass('static');
+			});
+	}
+})(jQuery);
+
+// other one
+$(function(){
+	
+	var note = $('#note'),
+		ts = new Date(2012, 0, 1),
+		newYear = true;
+	
+	if((new Date()) > ts){
+		// The new year is here! Count towards something else.
+		// Notice the *1000 at the end - time must be in milliseconds
+		ts = (new Date()).getTime() + 10*24*60*60*1000;
+		newYear = false;
+	}
+		
+	$('#countdown-1').countdown({
+		timestamp	: ts,
+		callback	: function(days, hours, minutes, seconds){
+			
+			var message = "";
+			
+			message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+			message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+			message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+			message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
+			
+			
+		}
+	});
+	
+});
+(function($){
+	
+	// Number of seconds in every time division
+	var days	= 24*60*60,
+		hours	= 60*60,
+		minutes	= 60;
+	
+	// Creating the plugin
+	$.fn.countdown = function(prop){
+		
+		var options = $.extend({
+			callback	: function(){},
+			timestamp	: 0
+		},prop);
+		
+		var left, d, h, m, s, positions;
+
+		// Initialize the plugin
+		init(this, options);
+		
+		positions = this.find('.position');
+		
+		(function tick(){
+			
+			// Time left
+			left = Math.floor((options.timestamp - (new Date())) / 1000);
+			
+			if(left < 0){
+				left = 0;
+			}
+			
+			// Number of days left
+			d = Math.floor(left / days);
+			updateDuo(0, 1, d);
+			left -= d*days;
+			
+			// Number of hours left
+			h = Math.floor(left / hours);
+			updateDuo(2, 3, h);
+			left -= h*hours;
+			
+			// Number of minutes left
+			m = Math.floor(left / minutes);
+			updateDuo(4, 5, m);
+			left -= m*minutes;
+			
+			// Number of seconds left
+			s = left;
+			updateDuo(6, 7, s);
+			
+			// Calling an optional user supplied callback
+			options.callback(d, h, m, s);
+			
+			// Scheduling another call of this function in 1s
+			setTimeout(tick, 1000);
+		})();
+		
+		// This function updates two digit positions at once
+		function updateDuo(minor,major,value){
+			switchDigit(positions.eq(minor),Math.floor(value/10)%10);
+			switchDigit(positions.eq(major),value%10);
+		}
+		
+		return this;
+	};
+
+
+	function init(elem, options){
+		elem.addClass('countdownHolder');
+
+		// Creating the markup inside the container
+		$.each(['Days','Hours','Minutes','Seconds'],function(i){
+			$('<span class="count'+this+'">').html(
+				'<span class="position">\
+					<span class="digit static">0</span>\
+				</span>\
+				<span class="position">\
+					<span class="digit static">0</span>\
+				</span>'
+			).appendTo(elem);
+			
+			if(this!="Seconds"){
+				elem.append('<span class="countDiv countDiv'+i+'"></span>');
+			}
+		});
+
+	}
+
+	// Creates an animated transition between the two numbers
+	function switchDigit(position,number){
+		
+		var digit = position.find('.digit')
+		
+		if(digit.is(':animated')){
+			return false;
+		}
+		
+		if(position.data('digit') == number){
+			// We are already showing this number
+			return false;
+		}
+		
+		position.data('digit', number);
+		
+		var replacement = $('<span>',{
+			'class':'digit',
+			css:{
+				top:'-2.1em',
+				opacity:0
+			},
+			html:number
+		});
+		
+		// The .static class is added when the animation
+		// completes. This makes it run smoother.
+		
+		digit
+			.before(replacement)
+			.removeClass('static')
+			.animate({top:'2.5em',opacity:0},'fast',function(){
+				digit.remove();
+			})
+
+		replacement
+			.delay(100)
+			.animate({top:0,opacity:1},'fast',function(){
+				replacement.addClass('static');
+			});
+	}
+})(jQuery);
+
+// other one
+$(function(){
+	
+	var note = $('#note'),
+		ts = new Date(2012, 0, 1),
+		newYear = true;
+	
+	if((new Date()) > ts){
+		// The new year is here! Count towards something else.
+		// Notice the *1000 at the end - time must be in milliseconds
+		ts = (new Date()).getTime() + 10*24*60*60*1000;
+		newYear = false;
+	}
+		
+	$('#countdown-2').countdown({
+		timestamp	: ts,
+		callback	: function(days, hours, minutes, seconds){
+			
+			var message = "";
+			
+			message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+			message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+			message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+			message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
+			
+			
+		}
+	});
+	
+});
+
+
+
+ 
+
+
+
+
+
+ 
+
