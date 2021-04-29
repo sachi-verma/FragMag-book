33d31b4a059dc2f4b9a424c1633862cd093605a7
Alisha <alisharao0749@gmail.com>
2021-04-29 18:45:31 +0530

    fourth commit

--- book.js
+++ book.js
@@ -1,118 +1,160 @@
-  $(window).load(function(){
-         $(".flipbook-viewport").css("visibility","visible");
-     })
+  $(window).load(function () {
+      $(".main-book").css("visibility", "visible");
+  })
+
+
+  function gradientRight() {
+
+      var i;
+      for (i = 2; i <= 7; i = i + 2) {
+          $("#" + i).addClass("gradient-right");
+
+      }
+
+  }
+  gradientRight();
+
+  function gradientLeft() {
+      var i;
+      for (i = 3; i <= 7; i = i + 2) {
+          $("#" + i).addClass("gradient-left");
+
+      }
+
+  }
+  gradientLeft();
+
+  var x = 0;
+  $(".zoom").click(function () {
+      x++;
+      if (x % 2 != 0) {
+          console.log('clicked');
+          $(".flipbook").css("transform", "scale(1)");
+      } else {
+          $(".flipbook").css("transform", "scale(0.7)");
+      }
+
+
+
+  })
+
+
+  function resizeViewport() {
+      var width = $(window).width(),
+          height = $(window).height();
+          $('.main-book').css({
+              width: width,
+              height: height
+          })
+           $('.flipbook-viewport').css({
+              width: width-300,
+          })
+        // if(width < 1022 && width > 500){
+        //     var x = (1022 - width)/2;
+        //   $(".flipbook").turn("size", 922-x, 600-x);
+        // }
+        if(width > height && width < 1024 ){
+            if(width < 700){
+                 $(".flipbook-viewport").css({"margin-left":-(800-width)});
+            }
+            
+             $(".flipbook").css({"transform":"scale(0.6)", "margin-top": -height/8});
+             $(".flipbook").turn("size", 722, 470);
+             $(".flipbook-viewport").css({"width": width-100,
+              "height": height });
+            
+              
+        
+        }
+        else{
+             $(".flipbook").turn("size", 922, 600);
+              $(".flipbook").css({"transform":"scale(0.8)", "margin-top": 0, "margin-bottom": "60px"});
+        }
+       
+
+
+
+  }
+
+  $(window).resize(function () {
+						resizeViewport();
+  })
+
+
  
- 
- function gradientRight() {
-     
-     var i;
-     for (i = 2; i <=7; i = i + 2) {
-         $("#" + i).addClass("gradient-right");
-
-     }
-
- }
- gradientRight();
-
- function gradientLeft() {
-    var i;
-     for (i = 3; i <=7; i = i + 2) {
-         $("#" + i).addClass("gradient-left");
-
-     }
-
- }
- gradientLeft();
-
-  var x = 0;  
-$(".zoom").click(function(){
-    x++;
-    if(x%2 != 0){
-        console.log('clicked');
-        $(".flipbook").css("transform", "scale(1)");
-    }
-    else{
-         $(".flipbook").css("transform", "scale(0.7)");
-    }
-    
-    
-    
-})
 
 
 
 
- function loadApp() {
-     var book = $('.flipbook');
-    
-    if (book.width() == 0 || book.height() == 0) {
-						setTimeout(loadApp, 10);
-						return;
-					}
+          function loadApp() {
+              var book = $('.flipbook');
 
-     // Create the flipbook
+              if (book.width() == 0 || book.height() == 0) {
+                  setTimeout(loadApp, 10);
+                  return;
+              }
 
-     $('.flipbook').turn({
-         // Width
+              // Create the flipbook
 
-         width: 922,
+              $('.flipbook').turn({
 
-         // Height
+                  width: 922,
 
-         height: 600,
+                  height: 600,
 
-         // Elevation
+                  elevation: 50,
 
-         elevation: 50,
+                  gradients: true,
 
-         // Enable gradients
+                  autoCenter: true,
 
-         gradients: true,
+              });
 
-         // Auto center this flipbook
+              $(".flipbook").css("transform", "scale(0.7)");
 
-         autoCenter: true,
+              $(".flipbook").bind("first", function (event) {
+                  $(".prev").css("visibility", "hidden");
+              });
 
-     });
-    
-     $( ".flipbook" ).css("transform", "scale(0.7)");
+              $(".flipbook").bind("last", function (event) {
+                  $(".next").css("visibility", "hidden");
+              });
 
-
-
-    //  $(".flipbook").bind("first", function(event) {
-    //      console.log('page 1 bitch');
-    //      $(this).addClass("translateCenter");
-
-    //  });
-    //   $(".flipbook").bind("last", function(event) {
-    //      console.log('page 1 bitch');
-    //      $(this).addClass("translateCenter");
-
-    //  });
-    
+              $(".prev").click(function () {
+                  $('.flipbook').turn('previous');
+                  $(".next").css("visibility", "visible");
+              })
+              $(".next").click(function () {
+                  $(".prev").css("visibility", "visible");
+                  $('.flipbook').turn('next');
+              })
 
 
+              resizeViewport();
 
-     $(document).keydown(function (e) {
 
-         var previous = 37,
-             next = 39;
+              $(document).keydown(function (e) {
 
-         switch (e.keyCode) {
-             case previous:
+                  var previous = 37,
+                      next = 39;
 
-                 $('.flipbook').turn('previous');
+                  switch (e.keyCode) {
+                      case previous:
 
-                 break;
-             case next:
+                          $('.flipbook').turn('previous');
+                          $(".next").css("visibility", "visible");
 
-                 $('.flipbook').turn('next');
+                          break;
+                      case next:
 
-                 break;
-         }
+                          $('.flipbook').turn('next');
+                          $(".prev").css("visibility", "visible");
 
-     });
-    
-    
- }
- loadApp();
+                          break;
+                  }
+
+              });
+
+
+          }
+          loadApp();
